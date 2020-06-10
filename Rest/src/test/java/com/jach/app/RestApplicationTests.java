4964595c5bce413114e2070380981f5abf2bfe91
julio <jcah549@gmail.com>
2020-06-09 23:23:35 -0500

    primer commit

---
+++ Rest/src/test/java/com/jach/app/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jach.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
